50ad28ddb25ee0ebf414778aab50d46a74f1a79d
Busboy <parrish.corbin5432@gmail.com>
2023-12-08 09:30:08 -0500

    Delete modules/pam.sh

--- modules/pam.sh
+++
@@ -1,6 +0,0 @@
-echo "Configuring pam"
-echo "Configured pam" >> pam.log
-sed -i 's/auth\trequisite\t\t\tpam_deny.so\+/auth\trequired\t\t\tpam_deny.so/' /etc/pam.d/common-auth
-sed -i '$a auth\trequired\t\t\tpam_tally2.so deny=5 unlock_time=1800 onerr=fail' /etc/pam.d/common-auth
-sed -i 's/sha512\+/sha512 remember=13/' /etc/pam.d/common-password
-read -p "Press [ENTER] key to continue" fakeenter
