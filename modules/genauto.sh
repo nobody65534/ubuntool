66b6a0b24b68f3333b8e5ccc6ba31072dff90237
Busboy <parrish.corbin5432@gmail.com>
2024-01-16 15:35:54 -0500

    Update genauto.sh

--- modules/genauto.sh
+++ modules/genauto.sh
@@ -3,9 +3,9 @@
 sed -i '1s/.*/timeout 3/' /boot/grub/menu.lst
 sed -i '2s/.*/password Greenbrier123!/' /boot/grub/menu.lst
 # Secure ctrl alt del
-echo 'root' > /etc/shutdown.allow
+echo 'root' >> /etc/shutdown.allow
 # Secure print screen/Magic SysRq
-echo 'kernel.sysrq = 0' > /etc/sysctl.conf
+echo 'kernel.sysrq = 0' >> /etc/sysctl.conf
 # Install harden
 apt-get install harden
 # Disable initramfs root prompt
