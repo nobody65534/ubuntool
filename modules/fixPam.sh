4708d72412bbfe5296ead760ce673ac1756be625
Busboy <parrish.corbin5432@gmail.com>
2024-01-16 15:36:59 -0500

    Update fixPam.sh

--- modules/fixPam.sh
+++ modules/fixPam.sh
@@ -20,24 +20,24 @@ rollback_changes() {
 # Updating PAM configuration files
 
 # Updating /etc/pam.conf
-echo "auth       required     pam_unix.so" > /etc/pam.conf || rollback_changes
+echo "auth       required     pam_unix.so" >> /etc/pam.conf || rollback_changes
 echo "account    required     pam_unix.so" >> /etc/pam.conf || rollback_changes
 echo "password   required     pam_unix.so" >> /etc/pam.conf || rollback_changes
 echo "session    required     pam_unix.so" >> /etc/pam.conf || rollback_changes
 
 # Updating /etc/pam.d/common-auth
-echo "auth       required     pam_faillock.so preauth silent audit deny=3 unlock_time=900" > /etc/pam.d/common-auth || rollback_changes
+echo "auth       required     pam_faillock.so preauth silent audit deny=3 unlock_time=900" >> /etc/pam.d/common-auth || rollback_changes
 echo "auth       [default=die] pam_faillock.so authfail audit deny=3 unlock_time=900" >> /etc/pam.d/common-auth || rollback_changes
 echo "auth       sufficient   pam_faillock.so authsucc audit deny=3 unlock_time=900" >> /etc/pam.d/common-auth || rollback_changes
 
 # Updating /etc/pam.d/common-account
-echo "account    required     pam_unix.so" > /etc/pam.d/common-account || rollback_changes
+echo "account    required     pam_unix.so" >> /etc/pam.d/common-account || rollback_changes
 
 # Updating /etc/pam.d/common-password
-echo "password   required     pam_unix.so" > /etc/pam.d/common-password || rollback_changes
+echo "password   required     pam_unix.so" >> /etc/pam.d/common-password || rollback_changes
 
 # Updating /etc/pam.d/common-session
-echo "session    required     pam_unix.so" > /etc/pam.d/common-session || rollback_changes
+echo "session    required     pam_unix.so" >> /etc/pam.d/common-session || rollback_changes
 
 # Restarting PAM-aware services
 service sshd restart || rollback_changes
