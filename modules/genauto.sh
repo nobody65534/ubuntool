fe4d408f28c183a9563e24d9829b0379a955008d
Busboy <parrish.corbin5432@gmail.com>
2024-01-16 15:23:58 -0500

    Update genauto.sh

--- modules/genauto.sh
+++ modules/genauto.sh
@@ -6,3 +6,7 @@ sed -i '2s/.*/password Greenbrier123!/' /boot/grub/menu.lst
 echo 'root' > /etc/shutdown.allow
 # Secure print screen/Magic SysRq
 echo 'kernel.sysrq = 0' > /etc/sysctl.conf
+# Install harden
+apt-get install harden
+# Disable initramfs root prompt
+sed '/GRUB_CMDLINE_LINUX/ s/$/ panic=0/' /etc/default/grub
